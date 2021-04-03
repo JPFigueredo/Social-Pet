212ab14721a28a7ebcdcc187547f7b23c854d840
Fabio Pelagaggi <fabio.pnunes@al.infnet.edu.br>
2021-04-03 03:45:18 -0300

    Version 0.01

---
+++ app/src/main/java/vortex/project/unify/Views/Activity/LoginActivity.kt
@@ -0,0 +1,16 @@
+package vortex.project.unify.Views.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import vortex.project.unify.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun startRegister(view: View) {}
+    fun logIN(view: View) {}
+}
---
+++ app/src/main/java/vortex/project/unify/Views/Activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package vortex.project.unify.Views.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import vortex.project.unify.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
