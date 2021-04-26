d12201c56152200481d8207490ac776af63f82e9
Fabio Pelagaggi <fabio.pnunes@al.infnet.edu.br>
2021-04-26 20:06:42 -0300

    Version 0.02 - Debug + NavHostFragment

--- app/src/main/java/vortex/project/unify/Views/Activity/MainActivity.kt
+++ app/src/main/java/vortex/project/unify/Views/Activity/MainActivity.kt
@@ -2,11 +2,14 @@ package vortex.project.unify.Views.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import vortex.project.unify.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.main_navigation_xml) as NavHostFragment? ?: return
     }
 }
