1148630be418718309d402b5f34a2518493ccc86
Luis <luis.barbosa@al.infnet.eu.br>
2021-05-13 16:54:58 -0300

    Remake Night Mode

--- app/src/main/java/vortex/project/unify/Views/Fragment/PostFragment.kt
+++ app/src/main/java/vortex/project/unify/Views/Fragment/PostFragment.kt
@@ -1,11 +1,14 @@
 package vortex.project.unify.Views.Fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toDrawable
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 import vortex.project.unify.R
 
@@ -24,9 +27,11 @@ class PostFragment : Fragment() {
     }
 
     private fun setToolbar() {
-        val background = context?.let { AppCompatResources.getDrawable(it, R.color.white) }
+        val value = TypedValue()
+        activity?.theme!!.resolveAttribute(R.attr.colorOnPrimary, value, true)
+        val background = context?.let { ContextCompat.getColor(it, value.resourceId) }
 
         activity?.toolbar_feed!!.visibility = View.VISIBLE
-        activity?.toolbar_feed!!.background = background
+        activity?.toolbar_feed!!.background = background!!.toDrawable()
     }
 }
