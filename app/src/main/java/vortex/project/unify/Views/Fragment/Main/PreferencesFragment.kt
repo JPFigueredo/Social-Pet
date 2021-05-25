2a882527c59e374b6a8f3ee0832fc0dc22725642
Luis <luis.barbosa@al.infnet.eu.br>
2021-05-25 15:09:19 -0300

    PreferencesViewModel + Action NightMode

--- app/src/main/java/vortex/project/unify/Views/Fragment/Main/PreferencesFragment.kt
+++ app/src/main/java/vortex/project/unify/Views/Fragment/Main/PreferencesFragment.kt
@@ -1,19 +1,24 @@
 package vortex.project.unify.Views.Fragment.Main
 
+import android.content.res.Configuration
 import android.os.Bundle
-import android.util.TypedValue
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.toDrawable
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_preferences.*
 import kotlinx.android.synthetic.main.toolbar.*
 import vortex.project.unify.R
+import vortex.project.unify.Views.ViewModel.PreferencesViewModel
+import java.util.*
 
 class PreferencesFragment : Fragment() {
+
+    private lateinit var preferencesViewModel: PreferencesViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -24,21 +29,48 @@ class PreferencesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        activity?.let {
+            act -> preferencesViewModel = ViewModelProviders.of(act).get(PreferencesViewModel::class.java)
+        }
+
         setToolbar()
+        setUpListeners()
+        fillUserData()
+    }
+
+    private fun setUpListeners() {
+        btn_changeApparence.setOnClickListener {
+            preferencesViewModel.modeNight.value = if (preferencesViewModel.modeNight.value == getString(R.string.day_mode)) {
+                getString(R.string.night_mode)
+            } else {
+                getString(R.string.day_mode)
+            }
+        }
+    }
+
+    private fun fillUserData() {
+        preferencesViewModel.modeNight.observe(viewLifecycleOwner, Observer {
+            if (it == getString(R.string.day_mode)) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                tv_appearance.text = it
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                tv_appearance.text = it
+            }
+        })
     }
 
     private fun setToolbar() {
-        (activity as AppCompatActivity).supportActionBar!!.setDisplayHomeAsUpEnabled(false)
-
-        val value = TypedValue()
-        activity?.theme!!.resolveAttribute(R.attr.colorOnPrimary, value, true)
-        val background = context?.let { ContextCompat.getColor(it, value.resourceId) }
+//        val value = TypedValue()
+//        activity?.theme!!.resolveAttribute(R.attr.colorOnPrimary, value, true)
+//        val background = context?.let { ContextCompat.getColor(it, value.resourceId) }
 
         activity?.toolbar_layout!!.visibility = View.VISIBLE
-        activity?.drawer_button!!.visibility = View.VISIBLE
-        activity?.camera_button!!.visibility = View.VISIBLE
-        activity?.message_button!!.visibility = View.VISIBLE
-        activity?.toolbar_layout!!.background = background!!.toDrawable()
+        activity?.drawer_button!!.visibility = View.GONE
+        activity?.camera_button!!.visibility = View.GONE
+        activity?.message_button!!.visibility = View.GONE
+        activity?.bottom_nav_post!!.visibility = View.GONE
+//        activity?.toolbar_layout!!.background = background!!.toDrawable()
 
         val parameter = activity?.toolbar!!.layoutParams as ViewGroup.MarginLayoutParams
         parameter.marginStart = 0
---
+++ app/src/main/java/vortex/project/unify/Views/ViewModel/PreferencesViewModel.kt
@@ -0,0 +1,10 @@
+package vortex.project.unify.Views.ViewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PreferencesViewModel : ViewModel() {
+
+    val modeNight = MutableLiveData<String>().apply { value = "Day Mode" }
+
+}
