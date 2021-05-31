f483137cc52d7287cc91936e72b13c6ddf26228a
João Figueredo <jpmf96@hotmail.com>
2021-05-31 20:22:49 -0300

    fragment ...New_Followers... renamed

--- app/src/main/java/vortex/project/unify/Views/Fragment/Main/NewFollowersFragment.kt
+++ app/src/main/java/vortex/project/unify/Views/Fragment/Main/NewFollowersFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import vortex.project.unify.R
 
-class NewFallowersFragment : Fragment() {
+class NewFollowersFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_fallowers, container, false)
+        return inflater.inflate(R.layout.fragment_new_followers, container, false)
     }
 
 }
