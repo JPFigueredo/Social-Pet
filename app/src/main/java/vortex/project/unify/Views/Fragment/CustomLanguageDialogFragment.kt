e1149bca8f9e77c658fb19a8b2ddb258fe2e4ac7
João Figueredo <jpmf96@hotmail.com>
2021-06-06 20:47:19 -0300

    Revert "language switch with CustomLanguageDialogFragment + PreferencesFragment.kt + PreferencesViewModel.kt, also, +strings.xml"
    This reverts commit 8364b9d7

--- app/src/main/java/vortex/project/unify/Views/Fragment/CustomLanguageDialogFragment.kt
+++ app/src/main/java/vortex/project/unify/Views/Fragment/CustomLanguageDialogFragment.kt
@@ -1,6 +1,6 @@
 package vortex.project.unify.Views.Fragment
 
-import  android.os.Bundle
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
